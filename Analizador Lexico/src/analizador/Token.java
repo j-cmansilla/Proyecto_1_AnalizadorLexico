552de09cc75beabec483070be151fe152f7d4e44
Jose Mansilla <jcmansillam@correo.url.edu.gt>
2018-03-30 15:33:04 -0600

    Creacion de la interfaz de usuario

---
+++ Analizador Lexico/src/analizador/Token.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package analizador;
+
+/**
+ *
+ * @author Carlos
+ */
+public enum Token {
+    CORRECTO, PALABRARESERVADA, OPERADORLOGICO, OPERADORARITMETICO, IDENTIFICADOR, VARIABLE, CONSTANTE, ESTRUCTURADECONTROL, VARIABLEPREDETERMINADA, FUNCION, COMENTARIO, ACCESOBASEDEDATOS,ERROR,TIPODEDATO
+}
