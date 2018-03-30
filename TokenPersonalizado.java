ff13a31b3ab128dc458eaebae78780ff81b4b119
Jose Mansilla <jcmansillam@correo.url.edu.gt>
2018-03-30 09:25:39 -0600

    Inicio del analizador lexico con JFlex

---
+++ TokenPersonalizado.java
@@ -0,0 +1,22 @@
+public class TokenPersonalizado {
+ 
+  String _token;
+  String _lexema;
+ 
+  public String getLexema(){
+    return this._lexema;
+  }
+ 
+  public String getToken(){
+    return this._token;
+  }
+ 
+  TokenPersonalizado (String lexema, String token){
+    this._lexema = lexema;
+    this._token = token;
+  }
+ 
+  public String toString(){
+    return "Lexema: " + this._lexema + " Token: " + this._token + ";";
+  }
+}
